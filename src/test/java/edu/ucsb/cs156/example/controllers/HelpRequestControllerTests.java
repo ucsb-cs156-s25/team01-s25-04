626f23f119060f92755e5aee382014347210cbd2
Fahim <zfahim540@gmail.com>
2025-04-25 18:39:17 -0700

    fz - Controller for HelpRequest along side test coverage and slight tweak on entity

---
+++ src/test/java/edu/ucsb/cs156/example/controllers/HelpRequestControllerTests.java
@@ -0,0 +1,143 @@
+package edu.ucsb.cs156.example.controllers;
+
+import edu.ucsb.cs156.example.repositories.UserRepository;
+import edu.ucsb.cs156.example.testconfig.TestConfig;
+import edu.ucsb.cs156.example.ControllerTestCase;
+import edu.ucsb.cs156.example.entities.HelpRequest;
+import edu.ucsb.cs156.example.entities.UCSBDate;
+import edu.ucsb.cs156.example.repositories.HelpRequestRepository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MvcResult;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
+import java.lang.runtime.ObjectMethods;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(controllers = HelpRequestController.class)
+@Import(TestConfig.class)
+
+public class HelpRequestControllerTests extends ControllerTestCase {
+        
+    @MockBean
+    HelpRequestRepository helpRequestRepository;
+
+    @MockBean
+    UserRepository userRepository;
+
+    // Authorization tests for /api/helprequest/admin/all
+
+    @Test
+    public void logged_out_users_cannot_get_all() throws Exception {
+            mockMvc.perform(get("/api/HelpRequest/all"))
+                            .andExpect(status().is(403)); // logged out users can't get all
+    }
+
+    @WithMockUser(roles = { "USER" })
+    @Test
+    public void logged_in_users_can_get_all() throws Exception {
+            mockMvc.perform(get("/api/HelpRequest/all"))
+                            .andExpect(status().is(200)); // logged
+    }
+
+    @Test
+    public void logged_out_users_cannot_post() throws Exception {
+            mockMvc.perform(post("/api/HelpRequest/post"))
+                            .andExpect(status().is(403));
+    }
+
+    @WithMockUser(roles = { "USER" })
+    @Test
+    public void logged_in_regular_users_cannot_post() throws Exception {
+            mockMvc.perform(post("/api/HelpRequest/post"))
+                            .andExpect(status().is(403)); // only admins can post
+    }
+
+    @WithMockUser(roles = { "USER" })
+    @Test
+    public void logged_in_user_can_get_all_helprequest() throws Exception {
+
+            // arrange
+            ZonedDateTime zdt1 = ZonedDateTime.parse("2022-01-03T00:00:00Z");
+
+            HelpRequest helpRequest1 = HelpRequest.builder()
+                            .requesterEmail("fahimzaman@ucsb.edu")
+                            .teamId("s22-5pm-3")
+                            .tableOrBreakoutRoom("7")
+                            .requestTime(zdt1)
+                            .explanation("Test1")
+                            .solved(false)
+                            .build();
+
+            ArrayList<HelpRequest> expectedHelpRequests = new ArrayList<>();
+            expectedHelpRequests.add(helpRequest1);
+
+            when(helpRequestRepository.findAll()).thenReturn(expectedHelpRequests);
+
+            // act
+            MvcResult response = mockMvc.perform(get("/api/HelpRequest/all"))
+                            .andExpect(status().isOk()).andReturn();
+
+            // assert
+
+            verify(helpRequestRepository, times(1)).findAll();
+            String expectedJson = mapper.writeValueAsString(expectedHelpRequests);
+            String responseString = response.getResponse().getContentAsString();
+            assertEquals(expectedJson, responseString);
+    }
+
+    @WithMockUser(roles = { "ADMIN", "USER" })
+    @Test
+    public void an_admin_user_can_post_a_new_helprequest() throws Exception {
+            // arrange
+
+            ZonedDateTime zdt1 = ZonedDateTime.parse("2022-01-03T00:00:00Z");
+
+            HelpRequest helpRequest1 = HelpRequest.builder()
+                            .requesterEmail("fahimzaman@ucsb.edu")
+                            .teamId("s22-5pm-3")
+                            .tableOrBreakoutRoom("7")
+                            .requestTime(zdt1)
+                            .explanation("Test1")
+                            .solved(false)
+                            .build();
+
+            when(helpRequestRepository.save(eq(helpRequest1))).thenReturn(helpRequest1);
+
+            // act
+            MvcResult response = mockMvc.perform(
+                            post("/api/HelpRequest/post?requesterEmail=fahimzaman@ucsb.edu&teamId=s22-5pm-3&tableOrBreakoutRoom=7&requestTime=2022-01-03T00:00:00Z&explanation=Test1&solved=false")
+                                                .with(csrf()))
+                            .andExpect(status().isOk()).andReturn();
+
+            // assert
+            verify(helpRequestRepository, times(1)).save(eq(helpRequest1));
+            String expectedJson = mapper.writeValueAsString(helpRequest1);
+            String responseString = response.getResponse().getContentAsString();
+            assertEquals(expectedJson, responseString);
+    }
+    
+}
